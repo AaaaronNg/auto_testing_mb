cdb3f6f20394d8f9937976b25d48c9e710214bc4
Aaron KL NG <aaron.kl.ng@ibm.com>
2023-05-16 11:25:09 +0800

    first commit

---
+++ src/main/java/com/example/SecurityJWT/SecurityConstants.java
@@ -0,0 +1,6 @@
+package com.example.SecurityJWT;
+
+public interface SecurityConstants {
+    public static final String JWT_KEY = "aafdfdsafadfasfasdfasfdsfsaffdsafdsafasfdsafdasfasffsdfasfafdas";
+    public static final String JWT_HEADER = "Authorization";
+}
---
+++ src/main/java/com/example/SecurityJWT/user/Role.java
@@ -0,0 +1,6 @@
+package com.example.SecurityJWT.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
